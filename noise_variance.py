75facc8142a603ea7e25c45d44e3f56ae5a70f05
PLOYWOW <90047405+PLOYWOW@users.noreply.github.com>
2022-02-11 22:58:38 +0900

    Create noise_variance.py

---
+++ noise_variance.py
@@ -0,0 +1,57 @@
+import os
+import cv2
+from matplotlib import pyplot
+
+VIDEO = ['A','B','C','D','E']
+
+for video in VIDEO:
+    img_filename_list = []
+    img_array = []
+    ave_array = []
+    var_array = []
+
+    img_filename_list = os.listdir(video)
+    img_num = len(img_filename_list)
+
+    for img_name in img_filename_list:
+        img_path = os.path.join(video, img_name)
+        img = cv2.imread(img_path,-1)
+        img = img.astype(float)/255
+        img_array.append(img)
+    
+    height = img_array[0].shape[0]
+    width = img_array[0].shape[1]
+
+    for j in range(height):
+        for k in range(width):
+            sum = 0
+            var = 0
+            for i in range(img_num):
+                sum += img_array[i][j][k]
+            ave = sum/img_num
+            ave_array.append(ave)
+            for i in range(img_num):
+                var += pow(img_array[i][j][k] - ave,2)
+            var = var/img_num
+            var_array.append(var)
+
+    pyplot.scatter(ave_array,var_array,c="black",marker="+")
+    pyplot.title(video)
+    pyplot.xlim([0.0,1.0])
+    pyplot.xlabel("Pixel Value ("+ video +")")
+    pyplot.ylim([0,0.0003])
+    pyplot.ylabel("Noise Variance ("+ video +")")
+    pyplot.savefig(video +".png")
+    pyplot.close()
+
+    pyplot.scatter(ave_array,var_array,c="black",marker="+")
+    pyplot.ticklabel_format(style='plain')
+    pyplot.title(video)
+    pyplot.xlim([0.0,1.0])
+    pyplot.xlabel("Pixel Value ("+ video +")")
+    pyplot.ylabel("Noise Variance ("+ video +")")
+    pyplot.savefig(video +"_full range.png")
+    pyplot.close()
+
+    
+
